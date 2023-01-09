9441ed03d89f77d184415ed9b95b0eba55f8b8b5
Kenny Chu <kennychu@asl.com.hk>
2023-01-09 12:09:56 +0800

    implemented general functions of dropdown menu

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Animated, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Animated, Dimensions, FlatList, Modal, StatusBar, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import { Carrot } from './Carrot';
+import { useHeaderHeight } from '@react-navigation/elements';
 
 type Props = {
   selected: string;
   setSelected: React.Dispatch<React.SetStateAction<string>>;
   placeholder?: string;
+  data: { label: string; value: string }[];
 };
+
+const ITEM_HEIGHT = 30;
 export const DropdownMenu = (props: Props) => {
-  const { selected, setSelected, placeholder: placeholder = 'Nothing Selected' } = props;
+  const { selected, setSelected, placeholder: placeholder = 'Nothing Selected', data } = props;
+  const headerHeight = useHeaderHeight();
+  const statusBarHeight = StatusBar.currentHeight || 0;
+  const flatListRef = useRef<FlatList>(null);
+
   const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<View>(null);
+  const [position, setPosition] = useState({
+    pageX: 0,
+    pageY: 0,
+    width: 0,
+    height: 0,
+  });
 
   function setMenuState() {
     if (isOpen) {
@@ -20,11 +35,24 @@ export const DropdownMenu = (props: Props) => {
   }
 
   function openMenu() {
+    ref.current?.measure((x, y, width, height, pageX, pageY) => {
+      setPosition({
+        width,
+        height,
+        pageX,
+        pageY,
+      });
+    });
     setIsOpen(true);
   }
 
   function closeMenu() {
     setIsOpen(false);
+  }
+
+  function onItemSelect(value: string) {
+    setSelected(value);
+    closeMenu();
   }
 
   const carrotAnimation = useRef(new Animated.Value(0)).current;
@@ -45,26 +73,69 @@ export const DropdownMenu = (props: Props) => {
     }
   }, [isOpen]);
   return (
-    <TouchableOpacity style={{ width: '100%' }} onPress={setMenuState}>
-      <View style={styles.container}>
-        <Text style={selected ? null : styles.placeholder}>{selected ? selected : placeholder}</Text>
-        <Animated.View
-          style={{
-            width: 15,
-            aspectRatio: 1,
-            transform: [
-              {
-                rotateZ: carrotAnimation.interpolate({
-                  inputRange: [0, 180],
-                  outputRange: ['0deg', '180deg'],
-                }),
-              },
-            ],
-          }}>
-          <Carrot />
-        </Animated.View>
-      </View>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity style={{ width: '100%' }} onPress={setMenuState}>
+        <View style={styles.container} ref={ref} onLayout={e => {}}>
+          <Text style={selected ? null : styles.placeholder}>{selected ? selected : placeholder}</Text>
+          <Animated.View
+            style={{
+              width: 15,
+              aspectRatio: 1,
+              transform: [
+                {
+                  rotateZ: carrotAnimation.interpolate({
+                    inputRange: [0, 180],
+                    outputRange: ['0deg', '180deg'],
+                  }),
+                },
+              ],
+            }}>
+            <Carrot />
+          </Animated.View>
+        </View>
+      </TouchableOpacity>
+      {isOpen && (
+        <Modal transparent={true} visible={isOpen}>
+          <TouchableWithoutFeedback onPress={closeMenu}>
+            <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.2)' }}>
+              <FlatList
+                ref={flatListRef}
+                onLayout={() => {
+                  flatListRef.current?.scrollToIndex({
+                    animated: false,
+                    index: data.findIndex(item => item.value === selected),
+                  });
+                }}
+                getItemLayout={(data, index) => ({
+                  length: ITEM_HEIGHT,
+                  offset: ITEM_HEIGHT * index,
+                  index,
+                })}
+                style={[
+                  styles.menu,
+                  {
+                    position: 'absolute',
+                    top: position.pageY + position.height,
+                    left: position.pageX,
+                    width: position.width,
+                  },
+                ]}
+                snapToInterval={ITEM_HEIGHT}
+                data={data}
+                renderItem={({ item }) => {
+                  return (
+                    <TouchableOpacity onPress={() => onItemSelect(item.value)}>
+                      <View style={[{ height: ITEM_HEIGHT, justifyContent: 'center', alignItems: 'center', width: '100%' }, selected === item.value ? styles.highlight : null]}>
+                        <Text>{item.label}</Text>
+                      </View>
+                    </TouchableOpacity>
+                  );
+                }}></FlatList>
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
+      )}
+    </>
   );
 };
 
@@ -83,5 +154,20 @@ const styles = StyleSheet.create({
   },
   placeholder: {
     color: 'grey',
+  },
+  menu: {
+    shadowColor: 'black',
+    shadowRadius: 5,
+    shadowOpacity: 0.2,
+    elevation: 5,
+    shadowOffset: { width: 0, height: 1 },
+    borderRadius: 10,
+    maxHeight: Math.floor(Dimensions.get('screen').height / 2 / ITEM_HEIGHT) * ITEM_HEIGHT,
+    zIndex: 2,
+    width: '100%',
+    backgroundColor: 'white',
+  },
+  highlight: {
+    backgroundColor: 'rgb(180,180,180)',
   },
 });
--- src/pages/DropdownMenuPage.tsx
+++ src/pages/DropdownMenuPage.tsx
@@ -5,9 +5,13 @@ import { DropdownMenu } from '../components/DropdownMenu';
 export const DropdownMenuPage = () => {
   const [displayText, setDisplayText] = useState('');
 
+  const DATA = [];
+  for (let i = 0; i < 10; i++) {
+    DATA.push({ label: 'item ' + i, value: 'item ' + i });
+  }
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginHorizontal: 20 }}>
-      <DropdownMenu selected={displayText} setSelected={setDisplayText} />
+      <DropdownMenu selected={displayText} setSelected={setDisplayText} data={DATA} />
     </SafeAreaView>
   );
 };

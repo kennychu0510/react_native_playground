a9596324a67077e73a86ddd9c408c8a0a8838d16
Kenny Chu <kennychu0510@gmail.com>
2023-01-26 15:46:52 +0800

    added .env sample

--- src/pages/Database.tsx
+++ src/pages/Database.tsx
@@ -1,11 +1,35 @@
-import { Alert, Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { Auth, Database as FirebaseDB } from '../firebase';
 import { onValue, ref, set } from 'firebase/database';
+import { RootScreenNavigationProp } from '../App';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Database() {
   const [data, setData] = useState([]);
   const [item, setItem] = useState('');
+  const navigation = useNavigation<RootScreenNavigationProp>();
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  function onEdit() {
+    setIsEditMode(true);
+  }
+  function onDone() {
+    if (Auth.currentUser) {
+      set(ref(FirebaseDB, 'items/' + Auth.currentUser.uid), {
+        items: JSON.stringify(data),
+      });
+    }
+    setIsEditMode(false);
+  }
+
+  React.useEffect(() => {
+    // Use `setOptions` to update the button that we previously specified
+    // Now the button includes an `onPress` handler to update the count
+    navigation.setOptions({
+      headerRight: () => (!isEditMode ? <Button title="Edit" onPress={onEdit} /> : <Button title="Done" onPress={onDone} />),
+    });
+  }, [navigation, Auth.currentUser, isEditMode, data]);
 
   useEffect(() => {
     if (!Auth.currentUser) return;
@@ -58,13 +82,19 @@ export default function Database() {
     ]);
   }
 
+  function onItemDelete(index: number) {
+    setData(data => data.filter((_, idx) => idx !== index));
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ flexDirection: 'row' }}>
-        <TextInput style={styles.input} placeholder="to do item" value={item} onChangeText={setItem} autoCapitalize="none"></TextInput>
-        <View style={styles.button}>
-          <Button onPress={onItemAdd} title={'add'}></Button>
-        </View>
+      <View style={{ flexDirection: 'row', marginHorizontal: 10, alignItems: 'center' }}>
+        <TextInput style={styles.input} placeholder="new item" value={item} onChangeText={setItem} autoCapitalize="none"></TextInput>
+        <TouchableOpacity onPress={onItemAdd}>
+          <View style={{ backgroundColor: '#298e46', borderRadius: 10, height: 30, justifyContent: 'center', paddingHorizontal: 10 }}>
+            <Text style={{ color: 'white', fontSize: 16 }}>Add</Text>
+          </View>
+        </TouchableOpacity>
       </View>
       <View style={{ marginHorizontal: 10, flex: 1, marginVertical: 10 }}>
         {data.length > 0 ? (
@@ -73,7 +103,14 @@ export default function Database() {
             renderItem={({ item, index }) => (
               <View key={index} style={styles.item}>
                 <Text style={styles.numberIndicator}>{index + 1}.</Text>
-                <Text>{item}</Text>
+                <Text style={{ fontSize: 20 }}>{item}</Text>
+                {isEditMode && (
+                  <TouchableOpacity onPress={() => onItemDelete(index)} style={{ marginLeft: 'auto' }}>
+                    <View style={{ backgroundColor: 'rgb(236,105,106)', borderRadius: 10, height: 30, justifyContent: 'center', paddingHorizontal: 10 }}>
+                      <Text style={{ color: 'white', fontSize: 16 }}>Delete</Text>
+                    </View>
+                  </TouchableOpacity>
+                )}
               </View>
             )}
           />
@@ -95,6 +132,8 @@ const styles = StyleSheet.create({
     margin: 5,
     backgroundColor: 'white',
     flex: 1,
+    marginRight: 10,
+    fontSize: 20,
   },
   button: {
     marginHorizontal: 5,
@@ -102,8 +141,11 @@ const styles = StyleSheet.create({
   item: {
     margin: 5,
     flexDirection: 'row',
+    alignItems: 'center',
+    height: 30,
   },
   numberIndicator: {
-    width: 20,
+    width: 25,
+    fontSize: 20,
   },
 });
